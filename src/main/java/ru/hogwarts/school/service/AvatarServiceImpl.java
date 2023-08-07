7e915f051a63bf9a42df97269770dc90e0306ab6
Sergei Driga <drigaserge@gmail.com>
2023-08-07 23:04:52 +0300

    hw4_done

--- src/main/java/ru/hogwarts/school/service/AvatarServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/AvatarServiceImpl.java
@@ -3,6 +3,7 @@ package ru.hogwarts.school.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.exception.StudentIsNotFound;
 import ru.hogwarts.school.model.Avatar;
 import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.repository.AvatarRepository;
@@ -16,40 +17,60 @@ import static java.nio.file.StandardOpenOption.CREATE_NEW;
 
 @Service
 public class AvatarServiceImpl implements AvatarService {
-private final StudentRepository studentRepository;
-private final AvatarRepository avatarRepository;
+    private final StudentRepository studentRepository;
+    private final AvatarRepository avatarRepository;
+
+    public AvatarServiceImpl(StudentRepository studentRepository, AvatarRepository avatarRepository) {
+        this.studentRepository = studentRepository;
+        this.avatarRepository = avatarRepository;
+    }
 
     @Value("${path.to.avatars.folder}")
     private String avatarsDir;
 
     @Override
     public void uploadAvatar(long studentId, MultipartFile avatarFile) throws IOException {
-        Student student = studentRepository.findById(studentId).get();
-        Path filePath = Path.of(avatarsDir, student.getName() + "." + getExtensions(avatarFile.getOriginalFilename()));
+        Student student = studentRepository
+                .findById(studentId)
+                .orElseThrow(StudentIsNotFound::new);
+        Path path = uploadToDisk(student, avatarFile);
+        uploadToDatabase(path, student, avatarFile);
+    }
+
+    private Path uploadToDisk(Student student, MultipartFile avatarFile) throws IOException {
+        Path filePath = Path.of(
+                avatarsDir,
+                student.getName() + "Student-" + student.getId() + "." + getExtensions(avatarFile.getOriginalFilename()));
         Files.createDirectories(filePath.getParent());
         Files.deleteIfExists(filePath);
         try (
                 InputStream is = avatarFile.getInputStream();
                 OutputStream os = Files.newOutputStream(filePath, CREATE_NEW);
                 BufferedInputStream bis = new BufferedInputStream(is, 1024);
-                BufferedOutputStream bos = new BufferedOutputStream(os, 1024);
+                BufferedOutputStream bos = new BufferedOutputStream(os, 1024)
         ) {
             bis.transferTo(bos);
         }
-        Avatar avatar = findAvatar(studentId);
-        avatar.setStudent(student);
-        avatar.setFilePath(filePath.toString());
-        avatar.setFileSize(avatarFile.getSize());
-        avatar.setMediaType(avatarFile.getContentType());
-        avatar.setData(avatarFile.getBytes());
-        avatarRepository.save(avatar);
+        return filePath;
     }
+    private void uploadToDatabase (Path filePath, Student student, MultipartFile avatarFile) throws IOException{
+
+            Avatar avatar = findAvatar(student.getId());
+            avatar.setStudent(student);
+            avatar.setFilePath(filePath.toString());
+            avatar.setFileSize(avatarFile.getSize());
+            avatar.setMediaType(avatarFile.getContentType());
+            avatar.setData(avatarFile.getBytes());
+            avatarRepository.save(avatar);
+        }
+
     private String getExtensions(String fileName) {
         return fileName.substring(fileName.lastIndexOf(".") + 1);
     }
 
     @Override
     public Avatar findAvatar(Long studentId) {
-        return null;
+        Avatar avatar = avatarRepository.findByStudent_Id(studentId);
+        return avatar == null ? new Avatar() : avatar;
     }
 }
